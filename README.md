c442a459e8497f7ffb7f32637ea4bbbfff7ff335
Jeslyn Tatang <jeslyn.tatang@gmail.com>
2019-04-10 01:08:48 +0000

    added api call

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Project Name
 
+
 ## Team Members
+Amanda Tan <br>
+Jeslyn Tatang
 
 ## Problem
+People often aren't sure what to watch 
 
 ## Opportunity
+Recommends movies to people based on their favorites
 
 ## Technical Magic
 
--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 /* global $ */
 
-console.log('hi');
-
-$('h1').click(function(){
-    $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
+var custom_api_url = "https://api.letterboxd.com/api/v0/auth/token";
+var custom2 = "https://api.openaq.org/v1/cities";
+$("#search").click(function() { 
+    $.ajax({
+        url: custom2,
+        method: "GET",
+        beforeSend:function(request) {
+            request.setRequestHeader("Access-Control-Allow-Origin", "*");
+            request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.setRequestHeader("Content-Type", "Accept: application/json");
+        },
+        crossDomain:true,
+        success: function(response) {
+            // YOUR CODE GOES HERE
+            console.log("test");
+        }
+    }); 
 });
